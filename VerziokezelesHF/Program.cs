6867dedfc5bbf2275581d69df63d86b0c00f05cf
dortmundia <csulienjatek@gmail.com>
2020-09-21 14:11:58 +0200

    project létrehozva!

--- VerziokezelesHF/Program.cs
+++ VerziokezelesHF/Program.cs
@@ -9,7 +9,19 @@ namespace VerziokezelesHF
     class Program
     {
         static void Main(string[] args)
-        {
+       {
+            //1.Hozz létre egy üres, konzolos projektet VerzioKezelesHF néven.
+            //2.Olvass be a felhasználótól 10 számot, számold ki az átlagukat!
+            //3.Módosítsd a programot, hogy a felhasználó adhassa meg, hány számot írjon be!
+            //4.Számold ki a számok közül a legnagyobbat és a legkisebbet!
+            //A részfeladatok külön commitban szerepeljenek! Vagyis:
+
+
+
+
+
+
+
         }
     }
 }
